880e0e9e2ed11fba9ff7531d46cd58605497434a
nwsmith13 <msmith.itc@gmail.com>
2026-05-23 19:51:14 -0500

    delete

--- src/components/ui/Card.jsx
+++
@@ -1,7 +0,0 @@
-export function Card({ children, className = "" }) {
-  return (
-    <div className={`rounded-lg border border-slate-100 bg-white/95 p-5 shadow-card ${className}`}>
-      {children}
-    </div>
-  );
-}
